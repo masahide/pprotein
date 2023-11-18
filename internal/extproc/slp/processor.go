0a40e392afcf06949ef68b9be49ec8cf2afbe150
ikura-hamu <104292023+ikura-hamu@users.noreply.github.com>
2023-11-18 18:41:16 +0900

    Revert ":sparkles: pt-query-digestも入れてみる"
    This reverts commit ffab1c6d5aaec3799ac4c786016c8559eded308f.

--- internal/extproc/slp/processor.go
+++ internal/extproc/slp/processor.go
@@ -1,12 +1,9 @@
 package slp
 
 import (
-	"bufio"
 	"bytes"
 	"fmt"
 	"io"
-	"log"
-	"os"
 	"os/exec"
 
 	"github.com/kaz/pprotein/internal/collect"
@@ -35,33 +32,5 @@ func (p *processor) Process(snapshot *collect.Snapshot) (io.ReadCloser, error) {
 		return nil, fmt.Errorf("external process aborted: %w", err)
 	}
 
-	cmd = exec.Command("pt-query-digest", "--type", "slowlog", bodyPath)
-	ptResult, err := cmd.Output()
-	if err != nil {
-		log.Printf("failed to execute pt-query-digest: %v", err)
-		return nil, nil //本物とは関係ない処理なのでエラーが出ても通す。
-	}
-	err = savePtResult("data/pt-"+snapshot.ID, ptResult)
-	if err != nil {
-		log.Printf("failed to save pt-query-digest result: %v", err)
-		return nil, nil //本物とは関係ない処理なのでエラーが出ても通す。
-	}
-
 	return io.NopCloser(bytes.NewBuffer(res)), nil
-}
-
-func savePtResult(path string, data []byte) error {
-	f, err := os.Create(path)
-	if err != nil {
-		return fmt.Errorf("failed to create file for pt result: %w", err)
-	}
-	defer f.Close()
-
-	w := bufio.NewWriter(f)
-	_, err = w.Write(data)
-	if err != nil {
-		return fmt.Errorf("failed to write pt result to file: %w", err)
-	}
-
-	return nil
 }
